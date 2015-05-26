c30a5477256e20c5d3c159daca8f313162ef7ed4
Dylan Richards <dylanerichards@gmail.com>
2015-05-26 13:22:07 -0400

    Clean up production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,12 +7,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = false
-
   config.active_support.deprecation = :log
-
   config.active_record.migration_error = :page_load
 
   config.assets.debug = true
-
   config.assets.raise_runtime_errors = true
+
+  config.react.variant = :development
 end
